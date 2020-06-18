ff455b711a8b75b62315a33eb18f6c5d095a311a
Evan Torrie <evantorrie@users.noreply.github.com>
2020-06-17 21:37:23 -0700

    Check in of another workflow for mod-the-cleaner

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	"go.opentelemetry.io/otel/api/key"
 )
 
 func main() {
 	fmt.Println("hello world");
 
+	x := key.Bool("just-a-test", true);
+	fmt.Printf("output a key: %v\n", x);
 }
